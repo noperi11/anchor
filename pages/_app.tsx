0e26d2dbeda1fb3ee1366b0a4595118e2dffdaa5
M Ilham Nofriansyah <126463961+noperi11@users.noreply.github.com>
2025-12-04 07:43:28 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import "./globals.css";
 
 
 export default function App({ Component, pageProps }: AppProps) {
