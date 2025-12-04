1bf0c3ad00eb95a2a0a710512b77250147a85322
M Ilham Nofriansyah <126463961+noperi11@users.noreply.github.com>
2025-12-04 07:41:28 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0d0d0d",
+        light: "#f5f5f5",
+        grayText: "#b3b3b3",
+      },
+    },
+  },
+  plugins: [],
+};
