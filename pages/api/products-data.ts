60f9496e9481c064ac7a5806db7d5dd93d294720
M Ilham Nofriansyah <126463961+noperi11@users.noreply.github.com>
2025-12-06 21:38:31 +0700

    Update products-data.ts

--- pages/api/products-data.ts
+++ pages/api/products-data.ts
@@ -34,7 +34,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // 2. FETCH PRODUCTS: Filter tabel Products berdasarkan BrandName
     const { data: productsData, error: productsError } = await supabase
       .from("products")
-      .select(ProductId,BrandName, ProductName, Category, ProductLink") // Select semua kolom yang relevan
+      .select("ProductId,BrandName, ProductName, Category, ProductLink") // Select semua kolom yang relevan
       .eq("BrandName", userBrandName); // Filter berdasarkan BrandName user yang login
 
     if (productsError) {
